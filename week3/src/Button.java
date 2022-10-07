b6b6726123c8cf7b82c10077e1578c2ae018e4df
Stefana <stefvnv@yahoo.com>
2022-10-07 17:21:17 +0100

    updated

--- week3/src/Button.java
+++ week3/src/Button.java
@@ -1,14 +1,15 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-public class Button implements ActionListener{
+
+public class Button implements ActionListener {
     JFrame window = new JFrame("example");
     JPanel panel = new JPanel();
 
     JButton bmiddle = new JButton("middle button");
     JButton bright = new JButton("right button");
     JButton bleft = new JButton("left button");
-    Label l = new Label("nothing clicked");
+    Label l = new Label("nothing clicked yet...");
 
     public Button() {
 
@@ -30,20 +31,21 @@ public class Button implements ActionListener{
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        // TODO Auto-generated method stub
 
-        if(e.getSource().equals(bleft)) {
-            l.setText("left button clicked");
+        if (e.getSource().equals(bleft)) {
+            panel.setBackground(Color.BLUE);
+            l.setText("LEFT button clicked");
         }
 
-        if (e.getSource().equals(bright)){
+        if (e.getSource().equals(bright)) {
             panel.setBackground(Color.RED);
+            l.setText("RIGHT button clicked");
         }
 
-        if (e.getSource().equals(bmiddle)){
+        if (e.getSource().equals(bmiddle)) {
             panel.setBackground(Color.GREEN);
+            l.setText("MIDDLE button clicked");
         }
-
 
 
     }
--- week3/src/Checkbox.java
+++ week3/src/Checkbox.java
@@ -9,28 +9,28 @@ public class Checkbox implements ActionListener {
     Panel p = new Panel();
     JFrame window = new JFrame("example");
 
-     public Checkbox() {
-         c.addActionListener(this);
-         c1.addActionListener(this);
+    public Checkbox() {
+        c.addActionListener(this);
+        c1.addActionListener(this);
 
-         p.add(c);
-         p.add(c1);
+        p.add(c);
+        p.add(c1);
 
-         window.getContentPane().add(p);
-         window.pack();
-         window.setVisible(true);
-     }
+        window.getContentPane().add(p);
+        window.pack();
+        window.setVisible(true);
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-         if (e.getSource().equals(c)){
-             if(c1.isSelected()) {
-                 c1.setSelected(false);
-             }
-         }
+        if (e.getSource().equals(c)) {
+            if (c1.isSelected()) {
+                c1.setSelected(false);
+            }
+        }
 
-        if (e.getSource().equals(c1)){
-            if(c.isSelected()) {
+        if (e.getSource().equals(c1)) {
+            if (c.isSelected()) {
                 c.setSelected(false);
             }
         }
