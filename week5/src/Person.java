49b0ee3283bdadcf91bc4dc68d232fb397140bc1
stefvnv laptop <a00282343@student.ait.ie>
2022-10-13 15:51:11 +0100

    Person class, week5 started

---
+++ week5/src/Person.java
@@ -0,0 +1,52 @@
+public class Person {
+    private String name;
+    private String address;
+    private int age;
+    private int rent;
+
+    //constructor
+    public Person() {
+        this.name = null;
+        this.address = null;
+        this.age = 0;
+        this.rent = 0;
+    }
+
+    //getters and setters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getRent() {
+        return rent;
+    }
+
+    public void setRent(int rent) {
+        this.rent = rent;
+    }
+
+    @Override
+    public String toString
+
+}
