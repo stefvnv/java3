86be2bb22d81a209d49f93f14fd5d0c00f3a5785
Stefana <stefvnv@yahoo.com>
2022-11-26 15:45:11 +0000

    research project - complete v2

--- project/src/Serialize.java
+++ project/src/Serialize.java
@@ -42,12 +42,13 @@ public class Serialize {
 
         clientList.add(new ClientModel("653", "Molly Anderson", "Sierra", "432 Rochester Drive, Athlone", "Dog", "Siberian Husky", "07/01/2017", "No"));
         clientList.add(new ClientModel("942", "Elliot Martin", "Matilda", "8692 Springfield, Dublin", "Cat", "Bengal", "23/06/2020", "Yes"));
-        clientList.add(new ClientModel("765", "George Black", "Wolfy", "42 River Street, Athlone", "Dog", "German Shepherd", "21/05/2015", "No"));
+        clientList.add(new ClientModel("765", "George Black", "Wolfy", "42 River St., Athlone", "Dog", "German Shepherd", "21/05/2015", "No"));
         clientList.add(new ClientModel("663", "Kerry Houston", "Robyn", "98 Animalview, Longford", "Guinea Pig", "Himalayan", "23/06/2022", "Yes"));
-        clientList.add(new ClientModel("138", "Peter Leen", "Spotty", "872 Cow Street, Ballymahon", "Dog", "Dalmatian", "05/08/2021", "No"));
-        clientList.add(new ClientModel("986", "Mary Perry", "Maxwell", "7 Patrick Street, Dublin", "Lizard", "Long-Tailed Gecko", "06/11/2021", "No"));
-        clientList.add(new ClientModel("904", "Ben Tenders", "Mittens", "982 Sunset Street, Mullingar", "Cat", "British Shorthair", "23/06/2017", "Yes"));
+        clientList.add(new ClientModel("138", "Peter Leen", "Spotty", "872 Cow St,, Ballymahon", "Dog", "Dalmatian", "05/08/2021", "No"));
+        clientList.add(new ClientModel("986", "Mary Perry", "Maxwell", "7 Patrick St., Dublin", "Lizard", "Long-Tailed Gecko", "06/11/2021", "No"));
+        clientList.add(new ClientModel("904", "Ben Tenders", "Mittens", "982 Sunset St., Mullingar", "Cat", "British Shorthair", "23/06/2017", "Yes"));
         clientList.add(new ClientModel("874", "Beatrice West", "Luna", "343 Bungalow Road, Athlone", "Dog", "Akita", "02/12/2022", "No"));
+        clientList.add(new ClientModel("496", "Bobby Brown", "Pooch", "67 Lord St., Dublin", "Horse", "Arabian", "29/08/2001", "No"));
 
         try {
             //Deserializing
